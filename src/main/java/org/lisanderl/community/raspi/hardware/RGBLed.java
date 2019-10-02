e643df7f7b58bb75c3a9f1277ab7b575441f6fbb
lisanderl <papalom31@gmail.com>
2019-10-02 22:22:54 +0300

    added controller

--- src/main/java/org/lisanderl/community/raspi/hardware/RGBLed.java
+++ src/main/java/org/lisanderl/community/raspi/hardware/RGBLed.java
@@ -1,10 +1,8 @@
 package org.lisanderl.community.raspi.hardware;
 
 import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.PinPullResistance;
 import com.pi4j.io.gpio.PinState;
-import com.pi4j.wiringpi.Gpio;
 import com.pi4j.wiringpi.SoftPwm;
 import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,23 +29,23 @@ public class RGBLed {
     private void init(){
         var gpio = GpioFactory.getInstance();
 
-        gpio.provisionDigitalOutputPin(pinModel.getRed())
+        gpio.provisionDigitalOutputPin(pinModel.getRedPin())
                 .setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);
-        gpio.provisionDigitalOutputPin(pinModel.getGreen())
+        gpio.provisionDigitalOutputPin(pinModel.getGreenPin())
                 .setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);
-        gpio.provisionDigitalOutputPin(pinModel.getBlue())
+        gpio.provisionDigitalOutputPin(pinModel.getBluePin())
                 .setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);
 
-        SoftPwm.softPwmCreate(pinModel.getRed().getAddress(), 0, 50);
-        SoftPwm.softPwmCreate(pinModel.getGreen().getAddress(), 0, 50);
-        SoftPwm.softPwmCreate(pinModel.getBlue().getAddress(), 0, 50);
+        SoftPwm.softPwmCreate(pinModel.getRedPin().getAddress(), 0, 50);
+        SoftPwm.softPwmCreate(pinModel.getGreenPin().getAddress(), 0, 50);
+        SoftPwm.softPwmCreate(pinModel.getBluePin().getAddress(), 0, 50);
     }
 
     public void showColor(Color color){
     var colorsArray = color.getRGBColorComponents(new float[3]);
-    SoftPwm.softPwmWrite(pinModel.getRed().getAddress(), (int) colorsArray[0]);
-    SoftPwm.softPwmWrite(pinModel.getGreen().getAddress(), (int) colorsArray[1]);
-    SoftPwm.softPwmWrite(pinModel.getBlue().getAddress(), (int) colorsArray[2]);
+    SoftPwm.softPwmWrite(pinModel.getRedPin().getAddress(), (int) colorsArray[0]);
+    SoftPwm.softPwmWrite(pinModel.getGreenPin().getAddress(), (int) colorsArray[1]);
+    SoftPwm.softPwmWrite(pinModel.getBluePin().getAddress(), (int) colorsArray[2]);
     }
 
 }
--- src/main/java/org/lisanderl/community/raspi/hardware/RGBPinModel.java
+++ src/main/java/org/lisanderl/community/raspi/hardware/RGBPinModel.java
@@ -4,21 +4,25 @@ import com.pi4j.io.gpio.Pin;
 import com.pi4j.io.gpio.RaspiPin;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
+
+import static org.apache.logging.log4j.LogManager.getLogger;
 
 @Data
 @Component
+@PropertySource("classpath:application.properties")
 public class RGBPinModel {
-    private final Pin red;
-    private final Pin blue;
-    private final Pin green;
+    private final Pin redPin;
+    private final Pin bluePin;
+    private final Pin greenPin;
 
-    public RGBPinModel(@Value("${raspi.led.red}") String red,
-                       @Value("${raspi.led.blue}") String blue,
-                       @Value("${raspi.led.green}") String green) {
-        this.red = RaspiPin.getPinByName(red);
-        this.blue = RaspiPin.getPinByName(blue);
-        this.green = RaspiPin.getPinByName(green);
+    public RGBPinModel(@Value("${raspi.led.red}") int red,
+                       @Value("${raspi.led.blue}") int blue,
+                       @Value("${raspi.led.green}") int green) {
+        this.redPin = RaspiPin.getPinByAddress(red);
+        this.bluePin = RaspiPin.getPinByAddress(blue);
+        this.greenPin = RaspiPin.getPinByAddress(green);
     }
 }
 
